18ca4dcbbac10bbf6845ed9f1703a917f5582018
Mohammed Sadiq <sidsmith9@gmail.com>
2019-03-22 23:35:46 +0530

    Add /api/customers router | rearrangement of lines

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // ------------ Imports ---------------
 const express = require('express');
 const helmet = require('helmet');
-
-const genres = require('./routes/genres');
-const root = require('./routes/landing');
 const startup = require('debug')('startup-log');
+
+const root = require('./routes/landing');
+const genres = require('./routes/genres');
+const customers = require('./routes/handler_customers');
 
 // ---------- Basic Setup -------------
 const app = express();
 app.use(express.json());
-
-
-app.use('/api/genres', genres);
-app.use('/', root);
 app.use(helmet());
+
+
+app.use('/', root);
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 // ------------ Logic -----------------
 const port = process.env.PORT || 3000;
