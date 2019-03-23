5b59dda6e8fa7809391ba2b160d7d506f83098b5
Mohammed Sadiq <sidsmith9@gmail.com>
2019-03-23 20:03:01 +0530

    Add Genre model and validateGenre

--- models/genre.js
+++ models/genre.js
@@ -6,20 +6,19 @@ const Genre = mongoose.model('Genre', new mongoose.Schema({
         type: String,
         required: true,
         minlength: 5,
-        maxlength: 50
+        maxlength: 50,
     }
 }));
 
 function validateGenre(genre) {
-    const schema = {
+    return Joi.validate(genre, {
         // Check if the name field :
         // 1. Is a string
         // 2. Has atleast 3 characters
         // 3. Is not empty
-        name: Joi.string().min(5).max(50).required()
-    };
-
-    return Joi.validate(genre, schema);
+        name: Joi.string().min(5).max(50).required(),
+        movies: Joi.array().min(1)
+    });
 }
 
 exports.Genre = Genre;
