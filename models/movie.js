f91dbf5fa75e63ef33b01693052eea7dd55ba654
m-zaink <sidsmith9@gmail.com>
2019-03-26 12:25:13 +0530

    Add genreSchema to genre attribute of movie.js

--- models/movie.js
+++ models/movie.js
@@ -1,36 +1,49 @@
-const mongoose = require('mongoose');
-const { Genre } = require('./genre');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const { genreSchema } = require("./genre");
+const Joi = require("joi");
 
 function validateMovie(movie) {
-    return Joi.validate(movie, {
-        title: Joi.string().required().min(1),
-        genre: Joi.required(),
-        numberInStock: Joi.number().default(0),
-        dailyRentalRate: Joi.number().default(1.0).min(1)
-    });
+  return Joi.validate(movie, {
+    title: Joi.string()
+      .required()
+      .min(1),
+    genre: Joi.required().string(),
+    numberInStock: Joi.number()
+      .min(0)
+      .max(100),
+    dailyRentalRate: Joi.number()
+      .min(1)
+      .max(50)
+  });
 }
 
-const Movie = mongoose.model('Movie', new mongoose.Schema({
+const Movie = mongoose.model(
+  "Movie",
+  new mongoose.Schema({
     title: {
-        type: String,
-        required: true,
-        min: 1
+      type: String,
+      required: true,
+      min: 1,
+      match: new RegExp("^[a-zA-Z0-9-][a-zA-Z0-9- ]*")
     },
     genre: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Genre'
+      type: genreSchema,
+      required: true
     },
     numberInStock: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 100
     },
     dailyRentalRate: {
-        type: Number,
-        default: 1.0,
-        min: 1.0
+      type: Number,
+      default: 1.0,
+      min: 1,
+      max: 50
     }
-}));
+  })
+);
 
 exports.Movie = Movie;
 exports.validateMovie = validateMovie;
