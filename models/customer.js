7753c4e2ff46c645cbc4b0a1219a8c08c9709f8c
m-zaink <sidsmith9@gmail.com>
2019-04-05 22:39:50 +0530

    Update Customer schema

--- models/customer.js
+++ models/customer.js
@@ -2,36 +2,41 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 function validateCustomer(customer) {
-    return Joi.validate(
-        customer,
-        {
-            name: Joi.string().required().min(3).max(50).regex(new RegExp('^[a-zA-Z]{3,50}$')),
-            email: Joi.string().required().regex(new RegExp('^[a-z0-9\.]+@gmail\.com$')),
-            premium: Joi.bool().default(false),
-        }
-    )
+	return Joi.validate(customer, {
+		name: Joi.string()
+			.required()
+			.min(3)
+			.max(50)
+			.regex(new RegExp('^[a-zA-Z][a-zA-Z ]{2,50}$')),
+		email: Joi.string()
+			.required()
+			.regex(new RegExp('^[a-z0-9.]+@gmail.com$')),
+		premium: Joi.bool().default(false)
+	});
 }
 
-const Customer = mongoose.model('Customer', new mongoose.Schema({
-    name: {
-        type: String,
-        require: true,
-        minlength: 3,
-        maxlength: 50,
-        match: new RegExp('^[a-zA-Z]{3,50}$')
-    },
-    email: {
-        type: String,
-        required: true,
-        match: new RegExp('^[a-z0-9\.]+@gmail\.com$'),
-        unique: true
-    },
-    premium: {
-        type: Boolean,
-        default: false
-    }
-}));
-
+const Customer = mongoose.model(
+	'Customer',
+	new mongoose.Schema({
+		name: {
+			type: String,
+			required: true,
+			minlength: 3,
+			maxlength: 50,
+			match: new RegExp('^[a-zA-Z][a-zA-Z ]{2,50}$')
+		},
+		email: {
+			type: String,
+			required: true,
+			match: new RegExp('^[a-z0-9.]+@gmail.com$'),
+			unique: true
+		},
+		premium: {
+			type: Boolean,
+			default: false
+		}
+	})
+);
 
 exports.Customer = Customer;
 exports.validateCustomer = validateCustomer;
